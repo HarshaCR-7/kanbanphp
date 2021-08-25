478494c1454a2475d1d609186c8bf1d40e67cb58
HarshaCR-7 <harshacr007@gmail.com>
2021-08-25 09:14:03 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -5,6 +5,12 @@ $(function() {
   }).disableSelection(); 
   $('.add-button').click(function() {
       var txtNewItem = $('#new_text').val();
-      $(this).closest('div.container').find('ul').append('<li class="card">'+txtNewItem+'</li>');
-  });    
+      $(this).closest('div.container').find('ul').append('<div><li class="card">'+txtNewItem+'</li><button class="remove"><i class= "fas fa-trash"></i></button></div>');
+  });   
+  $(".sortable").sortable({
+    connectWith: ".connectedSortable"
+  }).on("click",".remove", function(e){
+    e.preventDefault();
+    $(this).parent('div').remove();
+  })   
 });
